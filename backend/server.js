51a4e1603a555ea51a06bd0164f644c178a5a063
Arka Sarkar <arkaskr24@gmail.com>
2026-02-11 13:30:30 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { db } from "./config/firebaseConfig.js";
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 import userRouter from "./routes/userRoute.js";
 import blogRouter from "./routes/blogRoute.js";
 
@@ -10,18 +10,28 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://movish-admin.vercel.app",
+      "https://movish-admin.user.app",
+      "http://localhost:5173",
+      "http://localhost:5174",
+    ],
+    credentials: true,
+  }),
+);
 
 // Cloudinary Config
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET_KEY
-})
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
 
 // API Endpoints
-app.use('/api/user', userRouter);
-app.use('/api/blog', blogRouter);
+app.use("/api/user", userRouter);
+app.use("/api/blog", blogRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
